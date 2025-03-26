aa8b1a77f14c81b09d9c4455edb90ebee04216b8
liobarrozo <liobarrozo8@gmail.com>
2025-03-26 00:59:25 -0300

    CommitInicial

---
+++ albumSoda.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const url = "https://sodati-api.portfolio-ls.online/soda/";
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const albumContainer = document.getElementById("album-container");
+      data.forEach((album) => {
+        const albumCard = document.createElement("div");
+        albumCard.classList.add("album-card");
+        albumCard.innerHTML = `
+                    <h2>${album.title}</h2>
+                    <p>Fecha de lanzamiento: ${album.release_date}</p>
+                    <img src="${album.cover_medium}" alt="${album.title}">
+
+                    <div class="deezer-player">
+                        <iframe 
+                            src="https://widget.deezer.com/widget/dark/album/${album.id}" 
+                            width="300" 
+                            height="350" 
+                            frameborder="0" 
+                            allowtransparency="true" 
+                            allow="encrypted-media; clipboard-write" 
+                            scrolling="no">
+                        </iframe>
+                    </div>
+                `;
+        albumContainer.appendChild(albumCard);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+});

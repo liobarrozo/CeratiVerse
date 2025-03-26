2f2ef94225f9649951f2ba41c183d34e7d3ece58
liobarrozo <liobarrozo8@gmail.com>
2025-03-26 11:50:19 -0300

    Estructura de albums de Cerati lista, junto con la de soda

--- cerati/albums-cerati.js
+++ cerati/albums-cerati.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const url = "https://sodati-api.portfolio-ls.online/cerati/";
+  const url = "https://sodati-api.portfolio-ls.online/cerati";
 
   fetch(url)
     .then((response) => response.json())
@@ -8,23 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
       data.forEach((album) => {
         const albumCard = document.createElement("div");
         albumCard.classList.add("album-card");
-        albumCard.innerHTML = `
-                    <h2>${album.title}</h2>
-                    <p>Fecha de lanzamiento: ${album.release_date}</p>
-                    <img src="${album.cover_medium}" alt="${album.title}">
 
-                    <div class="deezer-player">
-                        <iframe 
-                            src="https://widget.deezer.com/widget/dark/album/${album.id}" 
-                            width="300" 
-                            height="350" 
-                            frameborder="0" 
-                            allowtransparency="true" 
-                            allow="encrypted-media; clipboard-write" 
-                            scrolling="no">
-                        </iframe>
-                    </div>
-                `;
+        // Ahora envolvemos el título en un enlace <a>
+        albumCard.innerHTML = `
+                  <a href="albums-cerati-id.html?id=${album.id}" class="album-link">
+                      <h2>${album.title}</h2>
+                  </a>
+                  <p>Fecha de lanzamiento: ${album.release_date}</p>
+                  <img src="${album.cover_medium}" alt="${album.title}">
+              `;
+
         albumContainer.appendChild(albumCard);
       });
     })

1b936d37497a1141880fb4b48057a687c51fba8b
mac <mac@MacBook-Air-de-mac.local>
2025-03-30 15:19:40 -0300

    FixSearch-2

--- search/search.js
+++ search/search.js
@@ -1,10 +1,8 @@
-// Redirigir a la página de búsqueda con el término de búsqueda
 function redirectToSearchPage() {
   const query = document.getElementById("search-bar").value.trim();
   if (query) {
-    window.location.href = `${window.location.origin}/search/search.html?query=${encodeURIComponent(
-      query
-    )}`;
+    // Asegurar que la ruta siempre sea absoluta desde la raíz del dominio
+    window.location.href = `${window.location.origin}/search/search.html?query=${encodeURIComponent(query)}`;
   } else {
     alert("Por favor ingresa un término de búsqueda.");
   }

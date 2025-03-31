dc6e823d579aed7e0b3e30c8c25f427e6c09099d
mac <mac@MacBook-Air-de-mac.local>
2025-03-31 00:55:13 -0300

    FixSearchPath y executeSearchWithEnter

--- search/search.js
+++ search/search.js
@@ -2,10 +2,19 @@
 function redirectToSearchPage() {
   const query = document.getElementById("search-bar").value.trim();
   if (query) {
-    window.location.href = `search/search.html?query=${encodeURIComponent(
-      query
-    )}`;
+    // Ruta relativa para GitHub Pages
+    window.location.href = `${window.location.origin}/CeratiVerse/search/search.html?query=${encodeURIComponent(query)}`;
   } else {
     alert("Por favor ingresa un término de búsqueda.");
   }
 }
+
+// Detectar cuando se presiona Enter en el campo de búsqueda
+document.getElementById("search-bar").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    redirectToSearchPage();
+  }
+});
+
+
+
